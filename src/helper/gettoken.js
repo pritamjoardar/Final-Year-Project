1a45a49cb78907596548ba627a45ddeac0448dbe
Akash Dey <dey899684@gmail.com>
2024-02-13 21:54:07 +0530

    change gettoken name

---
+++ src/helper/gettoken.js
@@ -0,0 +1,13 @@
+import { NextResponse,NextRequest } from "next/server";
+
+
+import jwt from "jsonwebtoken";
+export const getDataFromToken = (NextRequest)=>{
+    try {
+        const token = NextRequest.cookies.get('notes')?.value || '';
+        const decoded = jwt.verify(token, process.env.KEY );
+        return decoded._id;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
