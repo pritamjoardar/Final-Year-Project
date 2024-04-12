e2042310260db8d7b43cc6510084696e25b89788
Pritam Joardar <pritamjoardar200027@gmail.com>
2024-04-12 09:44:28 +0530

    addd comma

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -4,21 +4,21 @@ const Page = () => {
   return (
     <div className='flex flex-col gap-2 px-10 pt-2'>
         <h1 className='font-bold text-2xl'>Welcome to NotesFlex - Your Ultimate Note-Sharing Destination</h1>
-        <p>At NotesFlex&comma we believe in the power of knowledge sharing. Our platform serves as a vibrant hub where learners&comma educators&comma and knowledge enthusiasts converge to share&comma discover&comma and enrich their learning journeys.</p>
+        <p>At NotesFlex&comma; we believe in the power of knowledge sharing. Our platform serves as a vibrant hub where learners&comma; educators&comma; and knowledge enthusiasts converge to share&comma; discover&comma; and enrich their learning journeys.</p>
         <h1 className='font-bold text-xl'>Empowering Learning Communities</h1>
-        <p>NotesFlex is not just another note-sharing platform; it's a dynamic ecosystem designed to foster collaboration and facilitate the exchange of ideas. Whether you're a student looking for supplementary study materials&comma an educator aiming to share insights&comma or a curious mind eager to explore new horizons&comma you'll find a welcoming community here.</p>
+        <p>NotesFlex is not just another note-sharing platform; it's a dynamic ecosystem designed to foster collaboration and facilitate the exchange of ideas. Whether you're a student looking for supplementary study materials&comma; an educator aiming to share insights&comma; or a curious mind eager to explore new horizons&comma; you'll find a welcoming community here.</p>
         <h1 className='font-bold text-xl'>Features Tailored for You</h1>
         <p>Our platform offers a myriad of features to enhance your note-sharing experience:</p>
-        <span className="flex flex-col md:flex-row"><h1 className='font-bold text-xl'>Upload Notes: </h1><p>Seamlessly upload your notes&comma presentations&comma and study guides to share with others. Whether it's lecture summaries&comma exam prep materials&comma or research findings&comma your contributions are invaluable to the community.</p></span>
-        <span className="flex flex-col md:flex-row"><h1 className='font-bold text-xl'>Discover Notes: </h1><p> Dive into a vast repository of notes covering various subjects&comma topics&comma and disciplines. Explore diverse perspectives&comma gain new insights&comma and broaden your knowledge horizon.</p></span>
+        <span className="flex flex-col md:flex-row"><h1 className='font-bold text-xl'>Upload Notes: </h1><p>Seamlessly upload your notes&comma; presentations&comma; and study guides to share with others. Whether it's lecture summaries&comma; exam prep materials&comma; or research findings&comma; your contributions are invaluable to the community.</p></span>
+        <span className="flex flex-col md:flex-row"><h1 className='font-bold text-xl'>Discover Notes: </h1><p> Dive into a vast repository of notes covering various subjects&comma; topics&comma; and disciplines. Explore diverse perspectives&comma; gain new insights&comma; and broaden your knowledge horizon.</p></span>
         <span className="flex flex-col md:flex-row"><h1 className='font-bold text-xl'>View and Download: </h1><p>Access notes with ease and convenience. View them online or download them for offline reference. Our user-friendly interface ensures smooth navigation and hassle-free access to valuable resources.</p></span>
-        <span className="flex flex-col md:flex-row"><h1 className='font-bold text-xl'>Engage with Announcements: </h1><p>Stay updated with the latest announcements&comma events&comma and community news. From upcoming webinars and workshops to important notifications&comma our announcement section keeps you informed and connected.</p></span>
+        <span className="flex flex-col md:flex-row"><h1 className='font-bold text-xl'>Engage with Announcements: </h1><p>Stay updated with the latest announcements&comma; events&comma; and community news. From upcoming webinars and workshops to important notifications&comma; our announcement section keeps you informed and connected.</p></span>
         <h1 className='font-bold text-2xl'>Our Mission</h1>
-        <p>At NotesFlex&comma our mission is to democratize education and empower individuals to learn&comma grow&comma and succeed. By fostering a culture of knowledge sharing and collaboration&comma we strive to make learning accessible&comma inclusive&comma and enjoyable for all.</p>
+        <p>At NotesFlex&comma; our mission is to democratize education and empower individuals to learn&comma; grow&comma; and succeed. By fostering a culture of knowledge sharing and collaboration&comma; we strive to make learning accessible&comma; inclusive&comma; and enjoyable for all.</p>
         <h1 className='font-bold text-2xl'>Join Our Community</h1>
-        <p>Whether you're a student&comma educator&comma professional&comma or lifelong learner&comma you're invited to join our vibrant community. Connect with like-minded individuals&comma contribute your expertise&comma and embark on a journey of lifelong learning with NotesFlex.</p>
+        <p>Whether you're a student&comma; educator&comma; professional&comma; or lifelong learner&comma; you're invited to join our vibrant community. Connect with like-minded individuals&comma; contribute your expertise&comma; and embark on a journey of lifelong learning with NotesFlex.</p>
         <h1 className='font-bold text-2xl'>Get Started Today</h1>
-        <p>Ready to embark on your learning journey? Join [Platform Name] today and be part of a thriving community passionate about knowledge sharing&comma collaboration&comma and growth.</p>
+        <p>Ready to embark on your learning journey? Join [Platform Name] today and be part of a thriving community passionate about knowledge sharing&comma; collaboration&comma; and growth.</p>
     </div>
   )
 }
