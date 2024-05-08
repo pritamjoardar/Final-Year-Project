37752158581e2caa5190febbb371c2a01b262a70
shivamanand8969 <134687947+shivamanand8969@users.noreply.github.com>
2024-05-08 18:52:46 +0530

    update something

--- src/app/utils/Connect.ts
+++ src/app/utils/Connect.ts
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 export const ConnectDB =async() =>{ 
-// await mongoose.connect(process.env.URI)
-await mongoose.connect("mongodb://localhost:27017")
+await mongoose.connect(process.env.URI)
   .then(()=>console.log('Connected'))
   .catch((error:any)=>console.log(error))
 };
